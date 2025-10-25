2eace22f7454197bfb969cddeb2e4fd8c4eb505b
Swetony <swetonyancelmo@gmail.com>
2025-10-24 23:32:30 -0300

    chore: Inicializa projeto e exclui arquivos inútilizaveis

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// interface ICandidatura {
+//   id: string,
+//   tituloVaga: string,
+//   empresa: string,
+//   statusCandidatura: string
+// }
+
+function App() {
+  return (
+    <div>
+      <h1>JobTrack System</h1>
+    </div>
+  )
+}
+
+export default App
