5ea702ac793aa100bdc8560866e7e7414cc63fe6
Swetony <swetonyancelmo@gmail.com>
2025-10-25 11:32:43 -0300

    feat: Adiciona useEffect de buscar dados da API, e função de adicionar nova candidatura, cria formulário para nova candidatura e renderiza candidaturas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,126 @@
-// interface ICandidatura {
-//   id: string,
-//   tituloVaga: string,
-//   empresa: string,
-//   statusCandidatura: string
-// }
+import { useState, useEffect } from "react"
+import Select from 'react-select'
+
+interface IOption {
+  value: string;
+  label: string;
+}
+
+interface ICandidatura {
+  id: string,
+  tituloVaga: string,
+  empresa: string,
+  statusCandidatura: string
+}
 
 function App() {
+  const [candidaturas, setCandidatura] = useState<ICandidatura[]>([]);
+  const [tituloVaga, setTituloVaga] = useState<string>('');
+  const [empresa, setEmpresa] = useState<string>('');
+  const [statusCandidatura, setStatusCandidatura] = useState<IOption>({ value: 'PENDENTE', label: 'Pendente' });
+
+  const statusOptions: IOption[] = [
+    { value: 'PENDENTE', label: 'Pendente' },
+    { value: 'TRIAGEM', label: 'Triagem' },
+    { value: 'TESTE', label: 'Teste' },
+    { value: 'ENTREVISTA', label: 'Entrevista' },
+    { value: 'AGUARDANDO_RETORNO', label: 'Aguardando Retorno' },
+    { value: 'OFERTA', label: 'Oferta' },
+    { value: 'RECUSADA', label: 'Recusada' },
+    { value: 'CONTRATADO', label: 'Contratado' }
+  ];
+
+  const handleStatusChange = (newValue: IOption | null) => {
+    if (newValue) {
+      setStatusCandidatura(newValue);
+    }
+  };
+
+  useEffect(() => {
+    const buscarDados = async () => {
+      try {
+        const resposta = await fetch('http://localhost:8080/api/candidaturas');
+        const dados = await resposta.json();
+        setCandidatura(dados);
+      } catch (error) {
+        console.error('Erro ao buscar candidaturas: ', error);
+      }
+    }
+    buscarDados();
+  }, []);
+
+  const handleAdicionarCandidatura = async (evento: React.FormEvent) => {
+    evento.preventDefault();
+
+    if (!tituloVaga || !empresa || !statusCandidatura) {
+      alert('Candidatura incompleta, faça novamente!');
+      return;
+    }
+
+    try {
+      const resposta = await fetch('http://localhost:8080/api/candidaturas', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ tituloVaga: tituloVaga, empresa: empresa, statusCandidatura: statusCandidatura.value }),
+      });
+
+      if (!resposta.ok) {
+        throw new Error('Erro ao criar candidatura');
+      }
+
+      const novaCandidaturaCriada: ICandidatura = await resposta.json();
+
+      setCandidatura([...candidaturas, novaCandidaturaCriada]);
+      setTituloVaga('');
+      setEmpresa('');
+      setStatusCandidatura({ value: 'PENDENTE', label: 'Pendente' });
+    } catch (error) {
+      console.error('Erro ao cadastrar candidatura: ', error);
+    }
+  }
+
   return (
     <div>
       <h1>JobTrack System</h1>
+
+      <form onSubmit={handleAdicionarCandidatura}>
+        {/* <label htmlFor="tituloVaga">Título da vaga</label> */}
+        <input
+          type="text"
+          placeholder="Título da vaga"
+          value={tituloVaga}
+          onChange={(evento) => setTituloVaga(evento.target.value)}
+        />
+        {/* <label htmlFor="empresa">Nome da empresa</label> */}
+        <input
+          type="text"
+          placeholder="Nome da empresa"
+          value={empresa}
+          onChange={(evento) => setEmpresa(evento.target.value)}
+        />
+        {/* <label htmlFor="statusCandidatura">Status da candidatura: </label> */}
+        <Select
+          value={statusCandidatura}
+          onChange={handleStatusChange}
+          options={statusOptions}
+          className="react-select-container"
+          classNamePrefix="react-select"
+          placeholder="Selecione o status"
+        />
+        <button type="submit">
+          Adicionar
+        </button>
+      </form>
+
+      <ul>
+        {
+          candidaturas.map(candidatura => (
+            <li key={candidatura.id}>{candidatura.tituloVaga} - {candidatura.empresa} - {candidatura.statusCandidatura}</li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
